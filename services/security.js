ce0678011c581bfe863ac76552fbee7c71fea697
Ramoo Dexter <ramoo.dexter@gmail.com>
2017-09-05 15:47:08 +0200

    Add security setup

---
+++ services/security.js
@@ -0,0 +1,30 @@
+var passport = require('passport');
+var passportJWT = require('passport-jwt');
+var _ = require('lodash');
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+var accounts = require('../services/users');
+
+var jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+jwtOptions.secretOrKey = 'my-super-cool-secret-key';
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+
+  var account = accounts[_.findIndex(accounts, { id: jwt_payload.id })];
+  if (account) {
+    next(null, account);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+
+module.exports = {
+  passport: passport,
+  jwtOptions: jwtOptions
+}
